ee74e6d9df046332895f3db049fd8bec63c1fd7e
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-17 18:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+   let name = document.getELementById("fname").value;
+	   let lname = document.getELementById("lname").value;
 
+	alert(name,lname);
 }
